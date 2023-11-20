edc400add8a8f9b94863aa9ce9dfc7c63dc3c1e2
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2023-11-20 11:03:52 +0200

    String fixed

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using Analysis_Lab1.DataProcessors;
 using Analysis_Lab1.Utilities;
 
-var values = DataReader.LoadDataFromFile("C:\\Users\\Kirill-PC2\\Downloads\\data_lab1,2 1\\data_lab1,2\\25\\veib1.dat");
+var values = DataReader.LoadDataFromFile(@"C:\Users\Kirill-PC2\Downloads\data_lab1,2 1\data_lab1,2\25\veib1.dat");
 
 if (values == null)
 {
